784206745acbdccfc595a61c4876c5055c1be30d
Hugh Rawlinson <hugh@spotify.com>
2017-05-21 20:59:23 +0200

    Redisify

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const Boom = require('boom');
 const request = require('request');
 const process = require('process');
 
+const redis = require("redis")
+const redisClient = redis.createClient();
+
 const clientIdSecretPair = `${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`;
 const base64EncodedIdSecretPair = new Buffer(clientIdSecretPair).toString('base64');
 
@@ -36,10 +39,6 @@ function toQueryString(paramsObject) {
     .join('&') ;
 }
 
-let openRedirects = {
-  "yay": "http://localhost:4000"
-};
-
 const server = new Hapi.Server();
 server.connection({
   host: '0.0.0.0',
@@ -54,33 +53,37 @@ server.route({
   method: 'GET',
   path: '/spotifyOauthCallback',
   handler: function (req, reply) {
-    if (!req.query.state) {
-      // We didn't send state, our bad. This should've been caught earlier.
-      reply(Boom.badImplementation("Server Error 500"));
-    }
-    if (req.query.code) {
-      request({
-        method: "POST",
-        url: spotifyTokenUrl,
-        body: toQueryString({
-          "grant_type": "authorization_code",
-          "code": req.query.code,
-          "redirect_uri": encodeURI(resolveRedirectUri(req))
-        }),
-        headers: {
-          "Authorization": `Basic ${base64EncodedIdSecretPair}`,
-          "Content-Type": "application/x-www-form-urlencoded",
+    if (req.query.state) {
+      redisClient.get(req.query.state, (err, redisReply) => {
+        if (err) return reply(Boom.badRequest("Invalid State"));
+        if (req.query.code) {
+          request({
+            method: "POST",
+            url: spotifyTokenUrl,
+            body: toQueryString({
+              "grant_type": "authorization_code",
+              "code": req.query.code,
+              "redirect_uri": encodeURI(resolveRedirectUri(req))
+            }),
+            headers: {
+              "Authorization": `Basic ${base64EncodedIdSecretPair}`,
+              "Content-Type": "application/x-www-form-urlencoded",
+            }
+          }, (err, response, body) => {
+            if (err) {
+              reply(Boom.badImplementation("Failed request to Spotify Accounts"));
+            }
+            const jsonResponse = JSON.parse(body);
+            reply('x').redirect(`${redisReply.toString()}#${toQueryString(jsonResponse)}`);
+          });
+        } else {
+          // no code - user probably denied. Pass query on to client.
+          reply.redirect(`${redisReply.toString()}#${toQueryString(req.query)}`);
         }
-      }, (err, response, body) => {
-        if (err) {
-          reply(Boom.badImplementation("Failed request to Spotify Accounts"));
-        }
-        const jsonResponse = JSON.parse(body);
-        reply('x').redirect(`${openRedirects[req.query.state]}#${toQueryString(jsonResponse)}`);
       });
     } else {
-      // no code - user probably denied. Pass query on to client.
-      reply.redirect(`${openRedirects[req.query.state]}#${toQueryString(req.query)}`);
+      // We didn't send state, our bad. This should've been caught earlier.
+      reply(Boom.badImplementation("Server Error 500"));
     }
   }
 });
@@ -95,18 +98,24 @@ server.route({
           .map(scope => validScopes.indexOf(scope) > -1)
           .reduce((acc, el) => acc && el, true)
         if (scopesAreValid) {
-          const redirectUri = encodeURI(resolveRedirectUri(req));
-          return (params => reply.redirect(`${spotifyAuthUrl}?${toQueryString(params)}`))(
-            Object.assign({
-              "client_id": req.query.client_id,
-              "response_type": "code",
-              "redirect_uri": redirectUri,
-              // TODO: Implement the client redirect URI parameter
-              "state": "yay"
-            }, req.query.scope ? {
-              "scope": req.query.scope
-            } : {})
-          );
+          if (req.query.redirect_uri) {
+            const appRedirectUri = req.query.redirect_uri;
+            const redirectUri = encodeURI(resolveRedirectUri(req));
+            redisClient.set(req.id, appRedirectUri);
+            redisClient.expire(req.id, 60*5)
+            return (params => reply.redirect(`${spotifyAuthUrl}?${toQueryString(params)}`))(
+              Object.assign({
+                "client_id": req.query.client_id,
+                "response_type": "code",
+                "redirect_uri": redirectUri,
+                "state": req.id
+              }, req.query.scope ? {
+                "scope": req.query.scope
+              } : {})
+            );
+          } else {
+            return reply(Boom.badRequest("Invalid Redirect URI"));
+          }
         } else {
           return reply(Boom.badRequest("Invalid Scopes"));
         }
